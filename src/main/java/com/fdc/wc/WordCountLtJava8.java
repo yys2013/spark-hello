b02178642f1e4b71c87afbb47cb5b9f3d81ea3d8
matrix <matrix_2013@sina.com>
2017-07-08 13:35:22 +0800

    LTJAVA8

--- src/main/java/com/fdc/wc/WordCountLtJava8.java
+++ src/main/java/com/fdc/wc/WordCountLtJava8.java
@@ -1,5 +1,9 @@
 package com.fdc.wc;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 /**
  * 小于JAVA8版本实现
  *
@@ -11,7 +15,10 @@ public class WordCountLtJava8 {
 
     public static void main(String[] args) {
 
+        SparkConf conf = new SparkConf().setAppName("Spark WordCount Java8").setMaster("local");
+        JavaSparkContext sc = new JavaSparkContext(conf);
 
+        JavaRDD<String> lines = sc.textFile("E:\\logs\\Frankenstein.txt");
 
 
     }
