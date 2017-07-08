4121f3195d2ce86af353f477570aeea3fcfc5740
matrix <matrix_2013@sina.com>
2017-07-08 13:19:32 +0800

    LTJAVA8

---
+++ src/main/java/com/fdc/wc/WordCountLtJava8.java
@@ -0,0 +1,21 @@
+package com.fdc.wc;
+
+/**
+ * 小于JAVA8版本实现
+ *
+ * @author HE
+ * @version 1.0 2017-07-08
+ **/
+public class WordCountLtJava8 {
+
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+
+}
